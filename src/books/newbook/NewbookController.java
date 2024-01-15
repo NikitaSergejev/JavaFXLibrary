ee0d8a637933f52d6f0eea8caa39d09e564236df
pupil <pupil@ivkhk.ee>
2024-01-15 15:11:48 +0200

    Доделать сохранение книги в бд и добавить listbook

--- src/books/newbook/NewbookController.java
+++ src/books/newbook/NewbookController.java
@@ -6,7 +6,9 @@ package books.newbook;
  * and open the template in the editor.
  */
 
+import entity.Book;
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -16,6 +18,9 @@ import javafx.scene.control.TextField;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -23,24 +28,56 @@ import javafx.stage.Stage;
  * @author pupil
  */
 public class NewbookController implements Initializable {
+    private EntityManager em;
     private File selectedFile;
-    @FXML private TextField textField;
+    @FXML private TextField textFieldTitle;
     @FXML private Button btnSelectCover;
+    
+    @FXML
+    public void selectedCover() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выбор изображения");
+        
+        selectedFile = fileChooser.showOpenDialog(new Stage());
+        /*FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выбор обложки для книги");
+        selectedFile = fileChooser.showOpenDialog(new Stage());
+        btnSelectCover.setText("Выбран файл "+selectedFile.getName());
+        btnSelectCover.disableProperty().set(true);*/
+    }
+    @FXML
+    public void addNewBook(){
+        Book book = new Book();
+        book.setTitle(textFieldTitle.getText());
+        try(FileInputStream fis = new FileInputStream(selectedFile)){
+            byte[] fileContent = new byte[(int)selectedFile.length()];
+            fis.read(fileContent);
+            book.setCover(fileContent);
+            em.getTransaction().begin();
+            em.persist(book);
+            em.getTransaction().commit();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        btnSelectCover.disableProperty().set(false);
+        selectedFile = null;
+        textFieldTitle.setText("");
+        btnSelectCover.setText("Выбрать обложку книги");
+    }
+    
+     public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
     /**
      * Initializes the controller class.
      */
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }   
-    @FXML
-    public void selectedCover() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Выбор обложки для книги");
-        selectedFile = fileChooser.showOpenDialog(new Stage());
-        btnSelectCover.setText("Выбран файл "+selectedFile.getName());
-        btnSelectCover.disableProperty().set(true);
-    }
     
+    }   
 }
